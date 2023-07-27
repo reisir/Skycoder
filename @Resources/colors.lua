11b7b0892f9458530f9b5e65372429e6e0daca16
reisir <reisirito@gmail.com>
2023-07-28 00:42:02 +0300

    progress bar color override + use lua

--- @Resources/colors.lua
+++ @Resources/colors.lua
@@ -11,26 +11,59 @@ function split(color)
     return t
 end
 
-function setColor(i, numbers, prefix)
-    if numbers[4] == nil then
-        numbers[4] = '' .. 255
-    end
-    SKIN:Bang('!SetVariable', prefix .. 'r' .. i, trim(numbers[1]))
-    SKIN:Bang('!SetVariable', prefix .. 'g' .. i, trim(numbers[2]))
-    SKIN:Bang('!SetVariable', prefix .. 'b' .. i, trim(numbers[3]))
-    SKIN:Bang('!SetVariable', prefix .. 'a' .. i, trim(numbers[4]))
-end
+standby = {}
+active = {}
 
 function Initialize()
-
-    for i = 1, 5, 1 do
+    for i = 1, 6, 1 do
         color = SKIN:GetVariable('StandbyColor' .. i)
-        setColor(i, split(color), '')
-    end
-
-    for i = 1, 5, 1 do
+        table.insert(standby, sanitizeColor(color))
         color = SKIN:GetVariable('ActiveColor' .. i)
-        setColor(i, split(color), '_')
+        table.insert(active, sanitizeColor(color))
+    end
+end
+
+function sanitizeColor(color)
+    color = split(color)
+    if color[4] == nil then
+        color[4] = '' .. 255
     end
 
+    for i = 1, 4, 1 do
+        color[i] = tonumber(trim(color[i]))
+    end
+
+    return color
+end
+
+function component(i, s, a)
+    MeasureBand = SKIN:GetMeasure('MeasureBand' .. i)
+    band = MeasureBand:GetValue()
+    color = (s - (s - a) * band)
+
+    comma = ','
+    if i == 4 then
+        comma = ''
+    end
+
+    return color .. comma    
+end
+
+function calculateColor(n)
+    s = standby[n]
+    a = active[n]
+
+    color = ''
+    for i = 1, 4, 1 do
+        color = color .. component(i, s[i], a[i])
+    end
+
+    return color
+end
+
+function Update()
+    for i = 1, 6, 1 do
+        color = calculateColor(i)
+        SKIN:Bang('!SetVariable', 'Color' .. i, color)
+    end
 end
